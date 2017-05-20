635b29ea2b683d768d10b31231f24d892367e75d
gammaker <vav-94@mail.ru>
2017-05-20 15:05:52 +0300

    Heroku integration started

---
+++ Procfile.windows
@@ -0,0 +1,1 @@
+web: java -jar authmm/build/libs/authmm-1.0-SNAPSHOT.jar
---
+++ run-servers.bat
@@ -0,0 +1,2 @@
+java -jar authmm/build/libs/authmm-1.0-SNAPSHOT.jar
+java -jar gameserver/build/libs/gameserver-1.0-SNAPSHOT.jar
